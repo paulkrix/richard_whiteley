178bf92bcdf026e826c0915287487a28d9295814
Paul Krix <paul.krix@gmail.com>
2021-08-01 14:05:26 +1000

    udpate to styles

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 </div>
 <footer id="footer">
-<div id="copyright">
+<!-- <div id="copyright">
 &copy; <?php echo esc_html( date_i18n( __( 'Y', 'richardwhiteley' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 </div>
+-->
 </footer>
 </div>
 <?php wp_footer(); ?>
